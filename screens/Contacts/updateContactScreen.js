5c055d4e5a8213166a757db10105304903f7c6c6
Benjamin Rosales <benjamin.rosales@endeandina.bo>
2020-12-29 03:25:33 -0400

    profile update button delete

--- screens/Contacts/updateContactScreen.js
+++ screens/Contacts/updateContactScreen.js
@@ -1,17 +1,26 @@
 import React,{ 
     Component, 
     useState, 
-    useEffect 
+    useEffect,
+    useRef
 } from 'react';
-import { StyleSheet, View, Image,Text } from 'react-native';
+import { 
+    StyleSheet, 
+    View, 
+    Image,
+    Text 
+} from 'react-native';
+
 import TextInput from '../../components/TextInput';
 import Button from '../../components/button';
 import Background from '../../components/background';
 import { theme } from '../../core/theme'
-
 import * as Location from 'expo-location'
 import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import RBSheet from "react-native-raw-bottom-sheet";
 
+import { Avatar, ListItem, Icon } from "react-native-elements"
 import { ActivityIndicator, Colors, Switch } from 'react-native-paper';
 
 //DB
@@ -46,38 +55,91 @@ const storeContactScreen = (props) =>{
     //LOADING SCREEN
     const [loading, setLoading] = useState(true)
     
+    //Profile Picture
+    const refRBSheet = useRef();
+    const defaultImage = require('../../images/add-person.png');
+    const defaultImageUri = Image.resolveAssetSource(defaultImage).uri;
+    const [selectedImage, setSelectedImage] = useState({ localUri: defaultImageUri});
+    const [sizePicture] = useState({value: 200})
+
     const update = async () => {
+        try {
+            setLoading(true)  
+            let user = firebase.firebase.auth().currentUser
+            let contactId = props.route.params.userId
+            let dbRef = firebase.db.collection(user.email).doc(contactId);
 
-        let user = firebase.firebase.auth().currentUser
-        let contactId = props.route.params.userId
-        let dbRef = firebase.db.collection(user.email).doc(contactId);
+            let geoPoint = {};
 
-        let geoPoint = {};
-
-        if(isSwitchOn) {
-            geoPoint = {
-                latitude: location.latitude,
-                longitude: location.longitude
+            if(isSwitchOn) {
+                geoPoint = {
+                    latitude: location.latitude,
+                    longitude: location.longitude
+                }
+                await dbRef.update({
+                    name: name.value,
+                    lastname: lastname.value,
+                    alias: alias.value,
+                    number: number.value,
+                    email: email.value,
+                    location: geoPoint
+                })
+            } else {
+                await dbRef.update({
+                    name: name.value,
+                    lastname: lastname.value,
+                    alias: alias.value,
+                    number: number.value,
+                    email: email.value
+                })
             }
-            await dbRef.update({
-                name: name.value,
-                lastname: lastname.value,
-                alias: alias.value,
-                number: number.value,
-                email: email.value,
-                location: geoPoint 
-            })
-        } else {
-            await dbRef.update({
-                name: name.value,
-                lastname: lastname.value,
-                alias: alias.value,
-                number: number.value,
-                email: email.value
-            })
-        }
 
-        props.navigation.navigate('ViewContacts')
+            const imageUri = selectedImage.localUri;
+            let namePicture = props.route.params.userId;
+            if(imageUri != defaultImageUri) {
+                uploadImage(imageUri)
+                    .then(resolve => {
+                        //CREATE REFERENCES
+                        let ref = firebase.firebase.storage()
+                            .ref()
+                            .child(`Contacts/${user.email}/${namePicture}`)
+                        //UPLOAD FILE
+                        ref.put(resolve)
+                        .then(resolve =>{
+                            updateProfilePicture(props.route.params.userId)
+                        }).catch(error =>{
+                            console.log(error);
+                        });
+                    })
+                    .catch(error =>{
+                        console.log(error);
+                    });
+            }
+            props.navigation.navigate('ViewContacts')
+        } catch (e) {
+            console.log(e);
+            setLoading(false)
+        }
+    }
+    const updateProfilePicture = async (id) => {
+        let user = firebase.firebase.auth().currentUser
+        let dbRef = firebase.db.collection(user.email).doc(id)
+        firebase.firebase
+            .storage()
+            .ref(`Contacts/${user.email}/${dbRef.id}`)
+            .getDownloadURL()
+            .then(resolve =>{
+                let url = resolve
+                dbRef.update({
+                    profilePicture: url
+                })
+            })
+            .catch(error =>{
+                console.log(error);
+            }).finally(() => {
+                setLoading(false)
+                props.navigation.navigate('ViewContacts')
+            })
     }
 
     const  validateFields = () => {
@@ -106,11 +168,11 @@ const storeContactScreen = (props) =>{
     }
 
     const getContact = async (id) => {
+        urlImage();
         let user = firebase.firebase.auth().currentUser;
         let dbRef = firebase.db.collection(user.email).doc(id);
         let doc = await dbRef.get();
         let contact = doc.data();
-    
         setName({
             value: contact.name,
             error: ''
@@ -167,11 +229,120 @@ const storeContactScreen = (props) =>{
             <ActivityIndicator animating={true} color={Colors.red800} />
         )
     }
-        
+    function urlImage () {
+        let user = firebase.firebase.auth().currentUser;
+        let url = "";
+        firebase.firebase
+            .storage()
+            .ref(`Contacts/${user.email}/${props.route.params.userId}`)
+            .getDownloadURL()
+            .then(resolve =>{
+                url = resolve;
+                setSelectedImage({ localUri: url})
+                sizePicture.value = 200; 
+            })
+            .catch(error =>{
+                setSelectedImage({ localUri: defaultImageUri})
+                sizePicture.value = 150; 
+            })
+    }
+
+    function deleteImageFirebase() {
+        let id = props.route.params.userId
+        let user = firebase.firebase.auth().currentUser;
+        firebase.firebase
+            .storage()
+            .ref(`Contacts/${user.email}/${id}`)
+            .delete() 
+            .then(function() {
+                console.log("eliminado")
+                let dbRef = firebase.db.collection(user.email).doc(id)
+                dbRef.update({
+                    profilePicture: ''
+                })
+            })
+            .catch(function(error) {})
+    }
+
+    const uploadImage = (uri) => {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.onerror = reject;
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState === 4){
+                    resolve(xhr.response);
+                }
+            };
+
+            xhr.open("GET", uri);
+            xhr.responseType = "blob";
+            xhr.send();
+        });
+    };
+
+    const openImagePickerAsync = async () => {
+        const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    
+        if (permissionResult.granted === false) {
+            alert("Permiso denegado!");
+            return;
+        }
+
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+        if (pickerResult.cancelled === true) {
+            return;
+        }
+
+        sizePicture.value = 200;
+        setSelectedImage({ localUri: pickerResult.uri });
+        refRBSheet.current.close();
+    }
+    
+    const openCamareAsync = async () => {
+        const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    
+        if (permissionResult.granted === false) {
+            alert("Permiso denegado!");
+            return;
+        }
+
+        let pickerResult = await ImagePicker.launchCameraAsync();
+        if (pickerResult.cancelled === true) {
+            return;
+        }
+
+        sizePicture.value = 200;
+        setSelectedImage({ localUri: pickerResult.uri });
+        refRBSheet.current.close();
+    }
+
+    const deletePicture = () => {
+        setSelectedImage({ localUri: defaultImageUri});
+        deleteImageFirebase();
+        sizePicture.value = 150;
+        refRBSheet.current.close();
+    }
+
     return(
             <Background>
                 <View style={styles.container}>
-                    <Image source={require('../../images/user.jpg')} style={styles.image} />    
+                    <Avatar
+                        size="large"
+                        onPress={() => refRBSheet.current.open()}
+                        rounded
+                        source={{
+                            uri: selectedImage.localUri     
+                        }}
+                        containerStyle={{
+                            backgroundColor: 'grey',
+                            alignSelf: 'center',
+                            justifyContent: 'center'}}>
+                        <Avatar.Accessory  
+                                {...styles.Accessory}
+                                size={20}
+                                onPress={() => refRBSheet.current.open()}
+                            />
+                    </Avatar>    
                     <TextInput
                         label="Nombre"
                         returnKeyType="done"
@@ -266,6 +437,45 @@ const storeContactScreen = (props) =>{
                         Guardar Contacto
                     </Button>
                 </View>
+                <RBSheet
+                    ref={refRBSheet}
+                    closeOnDragDown={true}
+                    closeOnPressMask={false}
+                    height={sizePicture.value}
+                    customStyles={{
+                        wrapper: {
+                            backgroundColor: "transparent"
+                        },
+                        draggableIcon: {
+                            backgroundColor: "#000"
+                        }
+                    }}>
+                    <View>
+                        <ListItem onPress={openCamareAsync}>
+                            <Icon 
+                                name='camera'
+                                type='ionicon'
+                                />
+                            <ListItem.Content>
+                                <ListItem.Title>Tomar una Foto</ListItem.Title>
+                            </ListItem.Content>
+                        </ListItem>
+                        <ListItem onPress={openImagePickerAsync}>
+                            <Icon 
+                                name='image'
+                                type='ionicon'
+                                />
+                            <ListItem.Content>
+                                <ListItem.Title>Seleccionar una Foto</ListItem.Title>
+                            </ListItem.Content>
+                        </ListItem>
+                        <ListItem onPress={deletePicture}>
+                            <ListItem.Content>
+                                <ListItem.Title>Eliminar Foto</ListItem.Title>
+                            </ListItem.Content>
+                        </ListItem>
+                    </View>
+                </RBSheet>
             </Background>
     );
 }
